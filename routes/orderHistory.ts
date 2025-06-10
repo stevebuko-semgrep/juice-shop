3ddef6c2309712d4dc01cc405ed9901617aa2bad
stevebuko-semgrep <stephen@semgrep.com>
2025-06-10 13:41:03 -0400

    Update orderHistory.ts
    Updated line 36

--- routes/orderHistory.ts
+++ routes/orderHistory.ts
@@ -33,7 +33,48 @@ export function toggleDeliveryStatus () {
   return async (req: Request, res: Response, next: NextFunction) => {
     const deliveryStatus = !req.body.deliveryStatus
     const eta = deliveryStatus ? '0' : '1'
-    await ordersCollection.update({ _id: req.params.id }, { $set: { delivered: deliveryStatus, eta } })
+    //await ordersCollection.update({ _id: req.params.id }, { $set: { delivered: deliveryStatus, eta } })
+    import { type Request, type Response, type NextFunction } from 'express'
+    import { ObjectId } from 'mongodb' // Added import
+
+    import { ordersCollection } from '../data/mongodb'
+    import * as security from '../lib/insecurity'
+
+    export function orderHistory () {
+      return async (req: Request, res: Response, next: NextFunction) => {
+        const loggedInUser = security.authenticatedUsers.get(req.headers?.authorization?.replace('Bearer ', ''))
+        if (loggedInUser?.data?.email && loggedInUser.data.id) {
+          const email = loggedInUser.data.email
+          const updatedEmail = email.replace(/[aeiou]/gi, '*')
+          const order = await ordersCollection.find({ email: updatedEmail })
+          res.status(200).json({ status: 'success', data: order })
+        } else {
+          next(new Error('Blocked illegal activity by ' + req.socket.remoteAddress))
+        }
+      }
+    }
+
+    export function allOrders () {
+      return async (req: Request, res: Response, next: NextFunction) => {
+        const order = await ordersCollection.find()
+        res.status(200).json({ status: 'success', data: order.reverse() })
+      }
+    }
+
+    export function toggleDeliveryStatus () {
+      return async (req: Request, res: Response, next: NextFunction) => {
+        const deliveryStatus = !req.body.deliveryStatus
+        const eta = deliveryStatus ? '0' : '1'
+        // Ensure the id param is a valid ObjectId to prevent NoSQL injection
+        if (!ObjectId.isValid(req.params.id)) {
+          res.status(400).json({ status: 'error', message: 'Invalid order ID' })
+          return // Exit early if invalid
+        }
+        await ordersCollection.update({ _id: new ObjectId(req.params.id) }, { $set: { delivered: deliveryStatus, eta } })
+        res.status(200).json({ status: 'success' })
+      }
+    }
+    
     res.status(200).json({ status: 'success' })
   }
 }
